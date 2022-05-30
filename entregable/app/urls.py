4d1d973a2ad11325a3d7f31c023a79db5c4b8d6a
Mateo Calvi <mateocalvi2510@gmail.com>
2022-05-29 23:18:15 -0300

    Bug fixes

--- entregable/app/urls.py
+++ entregable/app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='Home'),
     path('driver', views.conductores, name='Drivers'),
     path('car', views.autos, name='Cars'),
 ]
